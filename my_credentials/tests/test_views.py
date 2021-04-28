64f2b8d78caf0a0e58833f65b294c4cbd7740d96
Bernhard Mallinger <bernhard.mallinger@eox.at>
2021-04-28 18:09:01 +0200

    Basic credentials create and update

--- my_credentials/tests/test_views.py
+++ my_credentials/tests/test_views.py
@@ -1,3 +1,4 @@
+import base64
 from contextlib import contextmanager
 from unittest import mock
 
@@ -8,10 +9,35 @@ from my_credentials.views import B64DecodedAccessDict
 
 
 @contextmanager
-def do_mock_secret_list(secrets):
+def do_mock_secret_list(secrets: list[k8s_client.V1Secret]):
     with mock.patch(
         "my_credentials.views.k8s_client.CoreV1Api.list_namespaced_secret",
         return_value=k8s_client.V1SecretList(items=secrets),
+    ) as mocker:
+        yield mocker
+
+
+@contextmanager
+def do_mock_secret_read(secret: k8s_client.V1Secret):
+    with mock.patch(
+        "my_credentials.views.k8s_client.CoreV1Api.read_namespaced_secret",
+        return_value=secret,
+    ) as mocker:
+        yield mocker
+
+
+@pytest.fixture()
+def mock_secret_create():
+    with mock.patch(
+        "my_credentials.views.k8s_client.CoreV1Api.create_namespaced_secret",
+    ) as mocker:
+        yield mocker
+
+
+@pytest.fixture()
+def mock_secret_patch():
+    with mock.patch(
+        "my_credentials.views.k8s_client.CoreV1Api.patch_namespaced_secret",
     ) as mocker:
         yield mocker
 
@@ -41,3 +67,67 @@ async def test_only_labelled_credentials_are_shown(client, secret):
         await client.get("/")
 
     assert "my-credentials" in mocker.mock_calls[0].kwargs["label_selector"]
+
+
+@pytest.mark.asyncio
+async def test_edit_credentials_shows_contents(client, secret):
+    with do_mock_secret_read(secret):
+        response = await client.get(f"/credentials-detail/{secret.metadata.name}")
+
+    assert "username" in response.text
+    assert B64DecodedAccessDict(secret.data)["username"] in response.text
+
+
+def create_form_data(is_update: bool):
+    return "&".join(
+        [
+            f"{k}={v}"
+            for k, v in [
+                ("secret_key", "user"),
+                ("secret_key", "pw"),
+                ("secret_value", "testington"),
+                ("secret_value", "supersecret"),
+            ]
+            + ([] if is_update else [("credentials_name", "new-secret")])
+        ]
+    )
+
+
+@pytest.mark.asyncio
+async def test_edit_credentials_updates_secrets(client, mock_secret_patch):
+    response = await client.post(
+        "/credentials-detail/existing-secret",
+        # NOTE: can't just pass form because async_asgi_testclient doesn't support
+        #       multidicts
+        data=create_form_data(is_update=True),
+        headers={"Content-Type": "application/x-www-form-urlencoded"},
+        allow_redirects=False,
+    )
+
+    kwargs = mock_secret_patch.mock_calls[0].kwargs
+    assert (
+        kwargs["body"].data["user"] == base64.b64encode("testington".encode()).decode()
+    )
+    assert kwargs["name"] == "existing-secret"
+
+    assert response.headers["location"] == "/"
+
+
+@pytest.mark.asyncio
+async def test_create_credentials_creates_secrets(client, mock_secret_create):
+    response = await client.post(
+        "/credentials-detail/",
+        # NOTE: can't just pass form because async_asgi_testclient doesn't support
+        #       multidicts
+        data=create_form_data(is_update=False),
+        headers={"Content-Type": "application/x-www-form-urlencoded"},
+        allow_redirects=False,
+    )
+
+    kwargs = mock_secret_create.mock_calls[0].kwargs
+    assert (
+        kwargs["body"].data["user"] == base64.b64encode("testington".encode()).decode()
+    )
+    assert kwargs["name"] == "new-secret"
+
+    assert response.headers["location"] == "/"
--- my_credentials/views.py
+++ my_credentials/views.py
@@ -1,14 +1,13 @@
 import base64
 import collections
 import logging
-import http
-from typing import cast
 
 from fastapi import Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from kubernetes import client as k8s_client, config as k8s_config
-import kubernetes.client.rest
+from pydantic import BaseModel
+from starlette.responses import RedirectResponse
 
 
 from my_credentials import app
@@ -23,7 +22,6 @@ LABEL_SELECTOR = "owner=edc-my-credentials"
 
 @app.on_event("startup")
 async def startup_load_k8s_config():
-    return
     try:
         k8s_config.load_kube_config()
     except Exception:
@@ -34,7 +32,6 @@ async def startup_load_k8s_config():
 @app.get("/", response_class=HTMLResponse)
 async def list_credentials(request: Request):
 
-    # TODO: add label selector
     secret_list: k8s_client.V1SecretList = (
         k8s_client.CoreV1Api().list_namespaced_secret(
             namespace=current_namespace(),
@@ -42,13 +39,7 @@ async def list_credentials(request: Request):
         )
     )
 
-    secrets_serialized = [
-        {
-            "name": secret.metadata.name,
-            "data": B64DecodedAccessDict(secret.data),
-        }
-        for secret in secret_list.items
-    ]
+    secrets_serialized = [serialize_secret(secret) for secret in secret_list.items]
 
     return templates.TemplateResponse(
         "credentials.html",
@@ -64,23 +55,69 @@ async def list_credentials(request: Request):
 async def credentials_detail(request: Request, credential_name: str = ""):
     is_new_credential = not bool(credential_name)
 
-    secret = {"name": credential_name, "data": {"a": "b", "c": "d"}}
+    if is_new_credential:
+        secret_data = {"name": "", "data": {}}
+    else:
+        secret: k8s_client.V1Secret = k8s_client.CoreV1Api().read_namespaced_secret(
+            name=credential_name,
+            namespace=current_namespace(),
+        )
+        secret_data = serialize_secret(secret)
 
     return templates.TemplateResponse(
         "credential_detail.html",
         {
             "request": request,
-            "secret": secret,
+            "secret": secret_data,
             "is_new_credential": is_new_credential,
         },
     )
 
 
-@app.post("/credentials-detail/{credential_name}", response_class=HTMLResponse)
+class CredentialsPayload(BaseModel):
+    credentials_name: str = ""
+    secret_value: list[str]
+    secret_key: list[str]
+
+
+@app.post("/credentials-detail/{credentials_name}", response_class=HTMLResponse)
 @app.post("/credentials-detail/", response_class=HTMLResponse)
-async def create_or_udpate(request: Request, credential_name: str = ""):
-    # credentials-name=asdf&secret_key=a&secret_value=b&secret_key=asdf&secret_value=d
-    ...
+async def create_or_update(request: Request, credentials_name: str = ""):
+
+    is_new_credential = not bool(credentials_name)
+
+    form_data = await request.form()
+    data = CredentialsPayload(
+        credentials_name=credentials_name or form_data.get("credentials_name"),
+        secret_value=form_data.getlist("secret_value"),
+        secret_key=form_data.getlist("secret_key"),
+    )
+
+    do_action = (
+        k8s_client.CoreV1Api().create_namespaced_secret
+        if is_new_credential
+        else k8s_client.CoreV1Api().patch_namespaced_secret
+    )
+
+    do_action(
+        name=data.credentials_name,
+        namespace=current_namespace(),
+        body=k8s_client.V1Secret(
+            data={
+                key: base64.b64encode(value.encode()).decode()
+                for key, value in zip(data.secret_key, data.secret_value)
+            }
+        ),
+    )
+
+    return RedirectResponse(url="/")
+
+
+def serialize_secret(secret: k8s_client.V1Secret) -> dict:
+    return {
+        "name": secret.metadata.name,
+        "data": B64DecodedAccessDict(secret.data),
+    }
 
 
 def current_namespace():
@@ -93,23 +130,3 @@ class B64DecodedAccessDict(collections.UserDict):
     def __getitem__(self, key) -> str:
         value = super().__getitem__(key)
         return base64.b64decode(value).decode()
-
-
-class SecretNotFound(RuntimeError):
-    pass
-
-
-def read_secret(name: str) -> k8s_client.V1Secret:
-    try:
-        return cast(
-            k8s_client.V1Secret,
-            k8s_client.CoreV1Api().read_namespaced_secret(
-                name=name,
-                namespace=current_namespace(),
-            ),
-        )
-    except kubernetes.client.rest.ApiException as e:
-        if e.status == http.HTTPStatus.NOT_FOUND:
-            raise SecretNotFound(f"Secret {name} not found") from e
-        else:
-            raise
